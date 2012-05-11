e22b00d81ef21d42cf0d6f78756397eca085162f
Reiko Shinozuka <reiko@opentech.co.jp>
2012-05-11 11:04:15 +0900

    modified title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def title(controller_name)
+    string = 'VPS MANAGE - ' + t("activerecord.models.#{controller_name.singularize}") + t("title.#{controller.action_name}")
+    string.html_safe
+  end
 end
